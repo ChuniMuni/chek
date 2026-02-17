ce42e548f3319268836bd3aae18c9f0db8ddbd17
Sherlock0731 <vitaliy0731@gmail.com>
2026-02-17 22:42:16 +0400

    Bug fix explanation

--- DevTest/features/step_definitions/test_steps.rb
+++ DevTest/features/step_definitions/test_steps.rb
@@ -36,27 +36,65 @@ def interrupt_unknown_exception(exception)
 end
 
 def try_to_find(xpath, delay, retry_number, parameters='')
-  e = ''
   counter = retry_number
+  
   while counter != 0
-    e = ''
-      begin
-        step %(Ожидаем #{delay} секунд)
-        if parameters!=''
-          page.find(:xpath,"#{xpath}", parameters)
-        else
-          page.find(:xpath,"#{xpath}")
-        end
-        # puts "Элемент #{xpath} найден с #{retry_number + 1 - counter} попытки"
-        counter = 0
-        result = true
-      rescue => e
-        p "Элемент #{xpath} не найден с #{retry_number + 1 - counter} попытки"
-        counter -= 1
-        result = false
-      rescue => e
-        interrupt_unknown_exception(e)
+    begin
+      step %(Ожидаем #{delay} секунд)
+      if parameters != ''
+        page.find(:xpath, "#{xpath}", parameters)
+      else
+        page.find(:xpath, "#{xpath}")
       end
+      # puts "Элемент #{xpath} найден с #{retry_number + 1 - counter} попытки"
+      return true
+    rescue Capybara::ElementNotFound => e
+      p "Элемент #{xpath} не найден с #{retry_number + 1 - counter} попытки"
+      counter -= 1
+      # Если это последняя попытка, возвращаем false
+      return false if counter == 0
+    rescue => e
+      # Обработка неожиданных исключений
+      p "Неожиданная ошибка при поиске элемента #{xpath}: #{e.class} - #{e.message}"
+      interrupt_unknown_exception(e)
+    end
   end
-  return result
+  
+  false
+end
+
+def try_to_find_improved(xpath, delay, retry_number, parameters = {})
+  retry_number.times do |attempt|
+    begin
+      sleep(delay) if delay > 0
+      
+      element = if parameters.empty?
+                  page.find(:xpath, xpath)
+                else
+                  page.find(:xpath, xpath, **parameters)
+                end
+      
+      puts "Элемент #{xpath} найден с #{attempt + 1} попытки" if ENV['DEBUG']
+      return true
+      
+    rescue Capybara::ElementNotFound
+      puts "Элемент #{xpath} не найден. Попытка #{attempt + 1} из #{retry_number}"
+      next # переходим к следующей итерации
+    rescue StandardError => e
+      puts "Критическая ошибка при поиске #{xpath}: #{e.class} - #{e.message}"
+      puts e.backtrace.first(5).join("\n") if ENV['DEBUG']
+      raise e # Reraise критических ошибок немедленно
+    end
+  end
+  
+  false
+end
+
+def wait_for_element(xpath, timeout: 10, **options)
+  begin
+    page.find(:xpath, xpath, wait: timeout, **options)
+    true
+  rescue Capybara::ElementNotFound
+    false
+  end
 end
