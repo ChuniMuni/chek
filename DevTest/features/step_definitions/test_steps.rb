cbac16050331948f4b9df859a5e3608457e41c6c
Sherlock0731 <84908129+Sherlock0731@users.noreply.github.com>
2026-02-17 22:57:22 +0400

    Update test_steps.rb

--- DevTest/features/step_definitions/test_steps.rb
+++ DevTest/features/step_definitions/test_steps.rb
@@ -35,34 +35,6 @@ def interrupt_unknown_exception(exception)
   raise exception.class
 end
 
-def try_to_find(xpath, delay, retry_number, parameters='')
-  counter = retry_number
-  
-  while counter != 0
-    begin
-      step %(Ожидаем #{delay} секунд)
-      if parameters != ''
-        page.find(:xpath, "#{xpath}", parameters)
-      else
-        page.find(:xpath, "#{xpath}")
-      end
-      # puts "Элемент #{xpath} найден с #{retry_number + 1 - counter} попытки"
-      return true
-    rescue Capybara::ElementNotFound => e
-      p "Элемент #{xpath} не найден с #{retry_number + 1 - counter} попытки"
-      counter -= 1
-      # Если это последняя попытка, возвращаем false
-      return false if counter == 0
-    rescue => e
-      # Обработка неожиданных исключений
-      p "Неожиданная ошибка при поиске элемента #{xpath}: #{e.class} - #{e.message}"
-      interrupt_unknown_exception(e)
-    end
-  end
-  
-  false
-end
-
 def wait_for_element(xpath, timeout: 10, **options)
   begin
     page.find(:xpath, xpath, wait: timeout, **options)
